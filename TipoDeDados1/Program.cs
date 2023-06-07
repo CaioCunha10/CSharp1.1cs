dc96230f7a4ff4f1e6bb7faf67fdc0857b6e0ff3
Caio Cunha <96320680+CaioCunha10@users.noreply.github.com>
2023-06-07 09:47:12 -0300

    first commit declaring variables numerics

---
+++ TipoDeDados1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Declarando variáveis numéricas");
+Console.WriteLine();
+// Declarando variáveis 
+
+byte valor1 = 255;
+sbyte valor2 = -127;
+int valor3 = -231231;
+uint valor4 = 2312354;
+long valor5 = -2312351;
+
+const int valor6 = 267;
+
+int var1 = 10, var2 = 30, var3 = 50;
+double var5 = 1.1;
+
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);
+Console.WriteLine(valor4);
+Console.WriteLine(valor5);
+Console.WriteLine(valor6);
+Console.WriteLine(var1);
+Console.WriteLine(var2);
+Console.WriteLine(var3);
+Console.WriteLine(var5);
+
+Console.ReadLine();
