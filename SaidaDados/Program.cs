6e464e4f8da6663e78449e520b1466ef4bb64b16
Caio Cunha <96320680+CaioCunha10@users.noreply.github.com>
2023-10-27 10:22:55 -0300

    Commit 1.0

---
+++ SaidaDados/Program.cs
@@ -0,0 +1,22 @@
+﻿int Idade = 11;
+string Nome = "Josemim";
+double Altura = 1.68;
+
+//nsole.WriteLine(Idade);
+//nsole.WriteLine(Nome);
+
+
+//Console.WriteLine(Nome);
+//Console.WriteLine("Tem");
+//Console.WriteLine(Idade);
+//Console.WriteLine("Anos");
+
+//Concatenação 
+//Console.WriteLine(Nome + "Tem" + Idade + "Anos luz");
+//Console.Write(Nome + "Tem" + Idade + "Anos luz");
+
+//Interpolação de strings usa-se $> a interpolação {}
+Console.WriteLine($"{Nome} tem idade {Idade} anus e {Altura} de comprimento");
+
+
+Console.ReadLine();
---
+++ TipoDeDados2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Atribuindo valores a double, float e decimal");
+Console.WriteLine();
+
+double n1 = 1.333;
+float n2 = 1.333f;
+decimal n3;
+
+n3 = 1.333m;
+
+Console.WriteLine(n1);
+Console.WriteLine(n2);
+Console.WriteLine(n3);
+
+Console.ReadLine();
+
+Console.WriteLine("Comparando a precisão de float, double e decimal");
+
+float x = 1f / 3f;
+double y = 1d / 3d;
+decimal z = 1m / 3m;
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+
+Console.ReadLine();
